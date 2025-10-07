43c4f3aed875961f4400d4871efff15aec61ff5f
jefferson22gs <jefferson.22gs@gmail.com>
2025-10-06 21:27:55 -0300

    Update sw.js

--- sw1.js
+++ sw1.js
@@ -1,17 +1,12 @@
-navigator.serviceWorker.register('/sw.js');
-const CACHE_NAME = 'pdv-v1';
-const urlsToCache = ['/', '/index.html'];
+const CACHE_NAME = 'pdv-cache-v1';
+const urlsToCache = ['/', '/index.html', '/favicon.ico'];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request)
-            .catch(() => caches.match(event.request))
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
